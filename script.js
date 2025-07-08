e852adf4c4ba18b77babe291827468b2e0c61a03
Ashish <ashishmasih92g@gmail.com>
2025-07-08 16:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // This is the boilerplate code given for you
 // You can modify this code
 // Product data
+
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -16,22 +17,72 @@ const productList = document.getElementById("product-list");
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
+	  const btn = document.createElement("button")
+	  btn.className="add-to-cart"
+	  btn.innerText = "Add to Cart";
+	  btn.id = `${product.id}`
+	  btn.addEventListener("click",addToCart)
+	  // btn.data-id=`${product.id}`
     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
-    productList.appendChild(li);
+    productList.append(li,btn);
   });
 }
 
 // Render cart list
-function renderCart() {}
+let cartList = document.getElementById("cart-list");
+function renderCart() {
+if(!sessionStorage.getItem("products")){
+	return
+}
+	let x = JSON.parse(sessionStorage.getItem("products"))
+	console.log(x)
+	for(let t of x){
+	let li = document.createElement("li");
+		let btns = document.createElement("button")
+		btns.innerText = "remove"
+	li.innerHTML = `${t.name} - ${t.price}`
+		li.append(btns)
+		cartList.append(li)
+		btns.addEventListener("click",removeFromCart)
+	}
+}
+	
 
 // Add item to cart
-function addToCart(productId) {}
+
+function addToCart(productId) {
+let newarr = []
+
+if(sessionStorage.getItem("products")){
+	let x = JSON.parse(sessionStorage.getItem("products"))
+	newarr= [...x]
+}
+	
+	let id = productId.target.id;
+	
+let items = products.find((v)=>{
+	return v.id==id
+})
+	newarr.push(items)
+	
+	// let jsobj = JSON.stringify(items)
+	sessionStorage.setItem("products", JSON.stringify([...newarr]));
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	let id = productId.target.id;
+
+	
+}
 
 // Clear cart
-function clearCart() {}
+let clearBtn = document.getElementById("clear-cart-btn")
+clearBtn.addEventListener("click",clearCart)
+function clearCart() {
+	cartList.innerText=""
+	sessionStorage.clear();
+}
 
 // Initial render
 renderProducts();
